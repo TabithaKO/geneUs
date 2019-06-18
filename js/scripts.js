7b54053bfa7937545d1246c7a46bd2678dc3910c
shack254 <123meshack@gmail.com>
2019-06-18 16:24:48 +0300

    initialized html file and created a function that counts the frequncy of nucleotides

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,32 @@
 function breakMaps() {
-    DNAlist = []
-    var lines = prompt("Enter a sequence: ")
-    var maplength = 4;
-    var char = lines.split("")
-    var length = char.length
+    var newList = [];
+    var DNAlist = [];
+    var lines = prompt("Enter a sequence: ");
+    var maplength = 3;
+    var char = lines.split("");
+    var length = char.length;
     for (i = 0; i + maplength < length; i++) {
-        var DNAslice = char.slice(i, maplength)
-        DNAlist.push(DNAslice)
-        maplength += 1
+        var DNAslice = char.slice(i, maplength);
+        DNAlist.push(DNAslice);
+        maplength += 1;
     }
-    var newList = []
-    DNAlist.forEach(function(group){
-        var joined = group.join("")
-        newList.push(joined)
+
+    DNAlist.forEach(function (group) {
+        var joined = group.join("");
+        newList.push(joined);
     })
-    
+
+    function createCounter() {
+        dnaCounter = {};
+        newList.forEach(function (nucleotide) {
+            if (nucleotide in dnaCounter) {
+                dnaCounter[nucleotide] = dnaCounter[nucleotide] += 1;
+            } else {
+                dnaCounter[nucleotide] = 1;
+            }
+            alert(dnaCounter[nucleotide]);
+        })
+    }
+    return createCounter();
 }
- function createCounter(newList){
-    dnaCounter = {}
-    newlist.forEach(function(nucleotide){
-        if(nucleotide in dnaCounter){
-            dnaCounter[nucleotide] = dnaCounter[nucleotide]+= 1
-        }else{
-            dnaCounter[nucleotide] = 1
-        }
-        alert(dnaCounter)
-    })
-}
+breakMaps()
